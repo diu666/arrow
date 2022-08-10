e925414711dac206b276a6f53dfa5fd5256baf38
DDD <760591590@qq.com>
2022-08-10 09:21:32 +0800

    add:新增移动端首页

---
+++ media/js/mobile-index.js
@@ -0,0 +1,20 @@
+/**
+ * 轮播
+ */
+var mySwiper = new Swiper(".main-carousel .swiper", {
+    loop: true, // 循环模式选项
+    pagination: {
+        el: ".swiper-pagination",
+        bulletClass: "carousel-cutsom-bullet",
+        bulletActiveClass: "carousel-custom-bullet-active",
+    },
+});
+
+var mySwiper = new Swiper(".live .swiper", {
+    loop: true, // 循环模式选项
+    pagination: {
+        el: ".swiper-pagination",
+        bulletClass: "carousel-cutsom-bullet",
+        bulletActiveClass: "carousel-custom-bullet-active",
+    },
+});
