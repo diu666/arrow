e08f406b042a9e1ba594df51a3e15fe3c4b0c5c8
DDD <760591590@qq.com>
2022-08-11 11:29:45 +0800

    update:修改移动端解决方案样式与交互逻辑

--- media/js/mobile-index.js
+++ media/js/mobile-index.js
@@ -23,56 +23,31 @@ var mySwiper = new Swiper(".live .swiper", {
  * 解决方案点击事件
  */
 
-// $('.solution-wrap li').each((index, item) => {
-//     $(item).click(function () {
-//         console.log(index, item, this)
-//         $(this)
-//             .css("border-bottom", "0.06rem solid #0084D5")
-//             .siblings()
-//             .css("border-bottom", "unset");
-//     })
-// });
-
-$(".solution-wrap li").click(function () {
-    $(this)
-        .css("border-bottom", "0.06rem solid #0084D5")
-        .siblings()
-        .css("border-bottom", "unset");
-    let title = $(this).find("span").text();
-    let css = null;
-    switch (title) {
-        case "AIOT":
-            css = ".aiot";
-            break;
-        case "工业控制":
-            css = ".plc";
-            break;
-        case "汽车电子":
-            css = ".car-elc";
-            break;
-        case "新能源":
-            css = ".new-energy";
-            break;
-    }
-    $(css).show().siblings().hide();
+$(".solution-wrap li").each((index, item) => {
+    $(item).click(function() {
+        $(this)
+            .css("border-bottom", "0.06rem solid #0084D5")
+            .siblings()
+            .css("border-bottom", "unset");
+        let content = $(this).parent().parent().next().children();
+        $(content[index]).show().css("display", "flex").siblings().hide();
+    });
 });
-
-
 
 /**
  * 技术咨询
  * 行业速递与技术热点点击事件
  */
 
- $('.technology-consult .select span').each((index, item) => {
-    $(item).click(function () {
-        $(this).addClass('select-active').siblings().removeClass('select-active')
+$(".technology-consult .select span").each((index, item) => {
+    $(item).click(function() {
+        $(this).addClass("select-active").siblings().removeClass("select-active");
         if (index == 0) {
-            $('.technology-consult .express').show();
-            $('.technology-consult .hotspot').hide()
+            $(".technology-consult .express").show();
+            $(".technology-consult .hotspot").hide();
         } else {
-            $('.technology-consult .express').hide();
-            $('.technology-consult .hotspot').show()
+            $(".technology-consult .express").hide();
+            $(".technology-consult .hotspot").show();
         }
-    })
-})
+    });
+});
