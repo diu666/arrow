d4c0d7c9daf7e558b0a4c09077705d998556ec88
DDD <760591590@qq.com>
2022-08-11 09:20:26 +0800

    update:修改样式、字体图标

--- media/js/index.js
+++ media/js/index.js
@@ -24,7 +24,18 @@ var mySwiper = new Swiper(".live .swiper", {
 /**
  * 解决方案点击事件
  */
-$(".solution li").click(function() {
+
+// $('.solution li').each((index, item) => {
+//     $(item).click(function () {
+//         console.log(index, item, this)
+//         $(this)
+//             .css("border-bottom", "4px solid #0084D5")
+//             .siblings()
+//             .css("border-bottom", "unset");
+//     })
+// });
+
+$(".solution li").click(function () {
     $(this)
         .css("border-bottom", "4px solid #0084D5")
         .siblings()
@@ -49,20 +60,40 @@ $(".solution li").click(function() {
     $(css).show().siblings().hide();
 });
 
+
+/**
+ * 技术咨询
+ * 行业速递与技术热点点击事件
+ */
+
+$('.technology-consult .select span').each((index, item) => {
+    $(item).click(function () {
+        $(this).addClass('select-active').siblings().removeClass('select-active')
+        if (index == 0) {
+            $('.technology-consult .express').show();
+            $('.technology-consult .hotspot').hide()
+        } else {
+            $('.technology-consult .express').hide();
+            $('.technology-consult .hotspot').show()
+        }
+    })
+})
+
+
 /**
  * 弹窗部分
  */
-$("#open").click(function(event) {
+$("#open").click(function (event) {
     event.stopPropagation();
     $("#popup").show();
 });
 
-$("#close").click(function() {
+$("#close").click(function () {
     $("#popup").hide();
 });
 
-//点击空白处隐藏弹出层，下面为滑动消失效果和淡出消失效果。
-$(document).click(function(event) {
+//点击空白处隐藏弹出层
+$(document).click(function (event) {
     var _con = $("#modal"); // 设置目标区域
     if (!_con.is(event.target) && _con.has(event.target).length === 0) {
         $("#popup").hide();
