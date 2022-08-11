e54926c0b276daeb5fe3d9d3f5e9ccfdd607f674
DDD <760591590@qq.com>
2022-08-11 11:07:16 +0800

    update:修改解决方案样式与交互逻辑

--- media/js/index.js
+++ media/js/index.js
@@ -25,75 +25,49 @@ var mySwiper = new Swiper(".live .swiper", {
  * 解决方案点击事件
  */
 
-// $('.solution li').each((index, item) => {
-//     $(item).click(function () {
-//         console.log(index, item, this)
-//         $(this)
-//             .css("border-bottom", "4px solid #0084D5")
-//             .siblings()
-//             .css("border-bottom", "unset");
-//     })
-// });
-
-$(".solution li").click(function () {
-    $(this)
-        .css("border-bottom", "4px solid #0084D5")
-        .siblings()
-        .css("border-bottom", "unset");
-    let title = $(this).find("span").text();
-    let css = null;
-    console.log(title);
-    switch (title) {
-        case "AIOT解决方案":
-            css = ".aiot";
-            break;
-        case "工业控制解决方案":
-            css = ".plc";
-            break;
-        case "汽车电子解决方案":
-            css = ".car-elc";
-            break;
-        case "新能源解决方案":
-            css = ".new-energy";
-            break;
-    }
-    $(css).show().siblings().hide();
+$(".solution li").each((index, item) => {
+    $(item).click(function() {
+        $(this)
+            .css("border-bottom", "4px solid #0084D5")
+            .siblings()
+            .css("border-bottom", "unset");
+        let content = $(this).parent().parent().next().children();
+        $(content[index]).show().css("display", "flex").siblings().hide();
+    });
 });
-
 
 /**
  * 技术咨询
  * 行业速递与技术热点点击事件
  */
 
-$('.technology-consult .select span').each((index, item) => {
-    $(item).click(function () {
-        $(this).addClass('select-active').siblings().removeClass('select-active')
+$(".technology-consult .select span").each((index, item) => {
+    $(item).click(function() {
+        $(this).addClass("select-active").siblings().removeClass("select-active");
         if (index == 0) {
-            $('.technology-consult .express').show();
-            $('.technology-consult .hotspot').hide()
+            $(".technology-consult .express").show();
+            $(".technology-consult .hotspot").hide();
         } else {
-            $('.technology-consult .express').hide();
-            $('.technology-consult .hotspot').show()
+            $(".technology-consult .express").hide();
+            $(".technology-consult .hotspot").show();
         }
-    })
-})
-
+    });
+});
 
 /**
  * 弹窗部分
  */
-$("#open").click(function (event) {
+$("#open").click(function(event) {
     event.stopPropagation();
     $("#popup").show();
 });
 
-$("#close").click(function () {
+$("#close").click(function() {
     $("#popup").hide();
 });
 
 //点击空白处隐藏弹出层
-$(document).click(function (event) {
+$(document).click(function(event) {
     var _con = $("#modal"); // 设置目标区域
     if (!_con.is(event.target) && _con.has(event.target).length === 0) {
         $("#popup").hide();
